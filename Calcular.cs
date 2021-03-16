2b2bb7e6f0b002a704d3759838684332c4d95c84
JOSEruben24 <80548962+JOSEruben24@users.noreply.github.com>
2021-03-16 00:08:56 -0600

    Add files via upload

---
+++ Calcular.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraWS
+{
+    public class Calcular
+    {
+        public decimal numero1 { get; set; }
+        public decimal numero2 { get; set; }
+
+        public decimal cuadrado (decimal n1 , decimal n2)
+        {
+            return n1 * n2;
+        }
+
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            return (n1 * n2)/2;
+
+        }
+
+        public float circulo(float n1, float n2)
+        {
+            
+           return n1*(n2*n2);           
+        }
+    }
+}
---
+++ CalcularWebService1.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalculadoraWS
+{
+    /// <summary>
+    /// Descripción breve de CalcularWebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularWebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public decimal cuadrado(decimal n1, decimal n2)
+        {
+            Calcular c = new Calcular();
+            return c.cuadrado(n1,n2);
+        }
+
+        [WebMethod]
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            Calcular c = new Calcular();
+            return c.triangulo(n1, n2);
+        }
+
+        [WebMethod]
+        public float circulo(float n1,float n2)
+        {
+            Calcular c = new Calcular();
+            return c.circulo(n1,n2);
+        }
+    }
+}
